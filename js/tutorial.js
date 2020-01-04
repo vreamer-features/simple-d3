3b2f07f45a7476f0b00250fb842ed455f55dd151
Vreamer <yhou.vreamer@gmail.com>
2020-01-04 22:45:34 +0800

    Create axis
    [#170464680]

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,26 +1,29 @@
-var dataset = [1, 2, 3, 4, 5];
+var data = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-var svgWidth = 500, svgHeight = 300, barPadding = 5;
-var barWidth = (svgWidth / dataset.length);
+var svgWidth = 500, svgHeight = 300;
 
 var svg = d3.select('svg')
     .attr('width', svgWidth)
     .attr('height', svgHeight)
 
+var xScale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, svgWidth])
+
 var yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset)])
-    .range([0, svgHeight])
+    .domain([0, d3.max(data)])
+    .range([svgHeight, 0])
 
+var x_axis = d3.axisBottom().scale(xScale)
 
-var barChart = svg.selectAll('rect')
-    .data(dataset)
-    .enter()
-    .append('rect')
-    .attr('y', (d) => svgHeight - yScale(d))
-    .attr('height', d => yScale(d))
-    .attr('width', barWidth - barPadding)
-    .attr('transform', (_, i) => {
-        var translate = [barWidth * i, 0]
-        return 'translate(' + translate + ')'
-    })
+var y_axis = d3.axisLeft().scale(yScale)
 
+svg.append('g')
+    .attr('transform', 'translate(50, 10)')
+    .call(y_axis)
+
+var xAxisTranslate = svgHeight - 20;
+
+svg.append('g')
+    .attr('transform', 'translate(50, ' + xAxisTranslate + ')')
+    .call(x_axis)
