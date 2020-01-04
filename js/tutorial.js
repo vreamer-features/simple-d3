20af92c2808640d566f958687c132d27193c0b15
Vreamer <yhou.vreamer@gmail.com>
2020-01-04 21:15:16 +0800

    Use scale
    [#170464680]

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,4 +1,4 @@
-var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+var dataset = [1, 2, 3, 4, 5];
 
 var svgWidth = 500, svgHeight = 300, barPadding = 5;
 var barWidth = (svgWidth / dataset.length);
@@ -7,23 +7,20 @@ var svg = d3.select('svg')
     .attr('width', svgWidth)
     .attr('height', svgHeight)
 
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, svgHeight])
+
+
 var barChart = svg.selectAll('rect')
     .data(dataset)
     .enter()
     .append('rect')
-    .attr('y', (d) => svgHeight - d)
-    .attr('height', d => d)
+    .attr('y', (d) => svgHeight - yScale(d))
+    .attr('height', d => yScale(d))
     .attr('width', barWidth - barPadding)
     .attr('transform', (_, i) => {
         var translate = [barWidth * i, 0]
         return 'translate(' + translate + ')'
     })
 
-var text = svg.selectAll('text')
-    .data(dataset)
-    .enter()
-    .append('text')
-    .text(d => d)
-    .attr('y', d => svgHeight - d - 2)
-    .attr('x', (_, i) => barWidth * (i + 0.25))
-    .attr('fill', '#A00000')
